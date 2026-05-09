d89b7969a8dc6674110ec42997b8f9b614ddecf7
Nihar M <1410nihar@gmail.com>
2026-05-09 15:19:40 +0530

    Add files via upload

---
+++ INSERT INTO new_registrations(full_name,.sql
@@ -0,0 +1,4 @@
+INSERT INTO new_registrations(full_name, email, course, enroll_date)
+VALUES ('RIYA PATEL', 'RIYA@GMAIL.COM', 'WEB_DEV', '20-6-2026'),
+('TARKOV', 'TARKOV@GMAIL.COM', 'PYTHON_DEVELOPER', '19-6-2026'),
+('ROHIT', 'ROHIT@GMAIL.COM', 'JAVA_DEV', '21-6-2026');
